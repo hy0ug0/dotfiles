017301866459c720c32dfd220ca68a15fef4e652
Hugo Contreras <hugocontreras@Hugos-MBP.asusrouter>
2020-07-31 10:19:13 +0200

    Add Vim plugins in submodules update

--- scripts/update-submodules.sh
+++ scripts/update-submodules.sh
@@ -3,3 +3,6 @@
 # Update DotBot & plugins
 git submodule update --remote dotbot
 git submodule update --remote dotbot-brew
+git submodule update --remote vim/plugged/night-owl.vim
+git submodule update --remote vim/plugged/vim-easy-align
+git submodule update --remote vim/plugged/vim-plug
